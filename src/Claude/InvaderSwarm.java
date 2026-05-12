30f4935ebbb54566457dd87a98fb554c67b561af
lassemarten <lasse.marten2007@gmail.com>
2026-05-12 09:32:25 +0200

    Max von 7 Reihen

--- src/Claude/InvaderSwarm.java
+++ src/Claude/InvaderSwarm.java
@@ -26,7 +26,7 @@ public class InvaderSwarm {
         invaders.clear();
         dx = Constants.INVADER_SPEED;
         shootTick = 0;
-        for (int row = 0; row < Math.floor(Constants.INVADER_ROWS + Math.pow((1.25 * (level-1)),0.5)); row++) { // Rechnung unschön verbaut
+        for (int row = 0; (row < Math.floor(Constants.INVADER_ROWS + Math.pow((1.25 * (level-1)),0.5)) && row <= 7); row++) { // Rechnung unschön verbaut
             for (int col = 0; col < Constants.INVADER_COLS; col++) {
                 int x = Constants.INVADER_START_X + col * (Constants.INVADER_WIDTH  + Constants.INVADER_H_GAP);
                 int y = Constants.INVADER_START_Y + row * (Constants.INVADER_HEIGHT + Constants.INVADER_V_GAP);
