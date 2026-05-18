e2e6a9a36aaf9cc4f2225f19f83d02014bdf2077
Benzen <marthe-catherine.benzen@std.dhsh.de>
2026-05-18 13:11:16 +0200

    - Position vom Ladebalken angepasst

--- src/main/java/de/game/util/Constants.java
+++ src/main/java/de/game/util/Constants.java
@@ -21,8 +21,8 @@ public final class Constants {
     public static final int PLAYER_Y       = SCREEN_HEIGHT - 70;
 
     // Dash
-    public static final int DASH_COOLDOWN  = 250;
-    public static final int DASH_Y = SCREEN_HEIGHT - 175;
+    public static final int DASH_COOLDOWN  = 200;
+    public static final int DASH_Y = SCREEN_HEIGHT - 650;
     public static final int DASH_X = SCREEN_WIDTH - 80;
 
     // Kugeln
