07259ca2b987ae27be0bec23541895ff391a806a
lassemarten <lasse.marten2007@gmail.com>
2026-06-22 14:40:44 +0200

    Toter Code raus aus Renderer

--- src/main/java/de/game/renderer/GameRenderer.java
+++ src/main/java/de/game/renderer/GameRenderer.java
@@ -130,24 +130,6 @@ public class GameRenderer {
                 continue;
             }
 
-//            // Fallback: altes gezeichnetes Invader-Design
-//            if (inv.getFarbe().equals("green")) {
-//                g2.setColor(COLOR_INVADER);
-//            } else if (inv.getFarbe().equals("red")) {
-//                g2.setColor(Color.RED);
-//            } else {
-//                g2.setColor(Color.BLUE);
-//            }
-//            g2.fillRoundRect(x, y, w, h, 8, 8);
-//
-//            g2.setColor(Color.BLACK);
-//            g2.fillOval(x + 7,  y + 8, 9, 9);
-//            g2.fillOval(x + 24, y + 8, 9, 9);
-//
-//            g2.setColor(new Color(30, 150, 30));
-//            g2.fillRect(x + 8,  y + 20, 5, 5);
-//            g2.fillRect(x + 17, y + 20, 5, 5);
-//            g2.fillRect(x + 26, y + 20, 5, 5);
         }
     }
 
@@ -158,15 +140,7 @@ public class GameRenderer {
         BufferedImage sprite = player.getSprite();
         if (sprite != null) {
             g2.drawImage(sprite, x, y, w, h, null);
-        } //else {
-//            // Fallback: altes Dreieck
-//            g2.setColor(COLOR_PLAYER);
-//            int[] px = { x + w / 2, x,      x + w };
-//            int[] py = { y - 20,    y + 10,  y + 10 };
-//            g2.fillPolygon(px, py, 3);
-//            g2.setColor(new Color(0, 180, 255));
-//            g2.fillRect(x + 5, y, w - 10, 12);
-//        }
+        }
     }
 
     private void renderBullets(Graphics2D g2, GameState state) {
